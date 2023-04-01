c10ca8722e5ab6474702878d01ae79210b4b02dd
andreitoledo <andreiltoledo@hotmail.com>
2023-04-01 03:34:27 -0300

    Definindo método de validação e executando a aplicação

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/andreitoledo/erudio/MathController.java
@@ -0,0 +1,46 @@
+package com.andreitoledo.erudio;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+		
+	private final AtomicLong counter = new AtomicLong();
+	/*
+	 * Lógica: Receber uma string, validar se ela é numérica, verificar o numero esta nulo
+	 * 
+	 * */
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
+			method=RequestMethod.GET)
+	public Double sum(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo
+			) throws Exception{
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();		
+		}
+		return ConvertToDouble(numberOne) + ConvertToDouble(numberTwo);
+		
+	}
+
+	private Double ConvertToDouble(String strNumber) {
+		if(strNumber == null) return 0D;
+		// BR 10,25 US 10.25
+		String number = strNumber.replaceAll(",", ".");
+		if(isNumeric(number)) return Double.parseDouble(number);
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if(strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+
+}
