884f32e049c663f61a4e7d64df55563b6092b1ce
andreitoledo <andreiltoledo@hotmail.com>
2023-04-01 17:16:46 -0300

    Os Verbos HTTP GET, POST,PUT e DELETE no Spring Framework e Spring Boot

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/andreitoledo/erudio/services/PersonServices.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java/src/main/java/com/andreitoledo/erudio/services/PersonServices.java
@@ -1,5 +1,7 @@
 package com.andreitoledo.erudio.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -17,8 +19,23 @@ public class PersonServices {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
-	public Person findById(String id) {
-		
+	/*
+	 * CRUD findById, findAll, create
+	 * */
+	// declara uma listagem de pessoas, intera em um array que vai de 0 a 8
+	// mocka uma pessoa para cada item dessa lista e adiciona a pessoa a listagem
+	public List<Person> findAll() {		
+		//logger.info("Finding all people!");		
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);			
+		}		
+		return persons;
+	}
+	
+	// lista por id - GET
+	public Person findById(String id) {			
 		logger.info("Finding one person!");
 		Person person = new Person();
 		// construindo um moc
@@ -26,8 +43,37 @@ public class PersonServices {
 		person.setFirstName("Andrei");
 		person.setLastName("Toledo");
 		person.setAddress("São Paulo - Capital - Brasil");
+		person.setGender("Male");				
+		return person;
+	}
+	
+	// create POST
+    public Person create(Person person) {
+    //	logger.info("Creating one person!");
+        return person;
+    }
+	
+	// update PUT
+    public Person update(Person person) {
+    // logger.info("Updating one person!");
+        return person;
+    }   
+    
+    // DELETE
+    public void delete(String id) {
+    // logger.info("Deleting one person!");
+        
+    }
+	
+	// lista GET
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		// construindo um moc
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some Address in Brasil " + i);
 		person.setGender("Male");
-				
 		return person;
 	}
 }
